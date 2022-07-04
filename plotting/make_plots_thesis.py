a0c1902f0ba12502742c9a6c1a0d73f00c3d4149
Max Oesterle <max_oesterle@web.de>
2022-07-04 14:35:29 +0200

    algorithms and optimisations experiment

--- plotting/make_plots_thesis.py
+++ plotting/make_plots_thesis.py
@@ -68,10 +68,11 @@ def read_measurement(bin, *args, **kwargs):
 
 
 def write_plt(filename, graph):
-    path = os.path.join(os.path.join(OUTPUT_PATH, graph))
+    # path = os.path.join(os.path.join(OUTPUT_PATH, graph))
+    path = os.path.join(os.path.join(OUTPUT_PATH))
 
-    if not os.path.exists(path):
-        os.makedirs(path)
+    # if not os.path.exists(path):
+    #     os.makedirs(path)
 
     plt.savefig(
         os.path.join(
@@ -129,7 +130,7 @@ def run_avg_all_times():
     name_ger = "thesis_avg_all-csp"
     name_ger_2 = "thesis_avg_all-csp_2"
     name_eur = "thesis_avg_mid-csp"
-    name_eur_2 = "thesis_avg_fast-csp_2"
+    name_eur_2 = "thesis_avg_mid-csp_2"
 
     avg_ger = read_measurement(name_ger + "-" + "parking_ger_hgv")
     avg_ger_2 = read_measurement(name_ger_2 + "-" + "parking_ger_hgv")
@@ -198,6 +199,169 @@ def run_avg_all_times():
         f.write(template)
 
 
+def run_median_all_times():
+    name_ger = "thesis_avg_all-csp"
+    name_ger_2 = "thesis_avg_all-csp_2"
+    name_eur = "thesis_avg_mid-csp"
+    name_eur_2 = "thesis_avg_mid-csp_2"
+
+    avg_ger = read_measurement(name_ger + "-" + "parking_ger_hgv")
+    avg_ger_2 = read_measurement(name_ger_2 + "-" + "parking_ger_hgv")
+
+    avg_eur = read_measurement(name_eur + "-" + "parking_europe_hgv")
+    avg_eur_2 = read_measurement(name_eur_2 + "-" + "parking_europe_hgv")
+
+    avg_ger = avg_ger.groupby(["algo"]).median()
+    avg_eur = avg_eur.groupby(["algo"]).median()
+    avg_ger_2 = avg_ger_2.groupby(["algo"]).median()
+    avg_eur_2 = avg_eur_2.groupby(["algo"]).median()
+
+    avg_all = avg_ger.join(avg_eur, rsuffix="_eur")[["time_ms", "time_ms_eur"]]
+    avg_all_2 = avg_ger_2.join(avg_eur_2, rsuffix="_eur")[["time_ms", "time_ms_eur"]]
+
+    avg_all["time_ms_2"] = avg_all_2["time_ms"]
+    avg_all["time_ms_eur_2"] = avg_all_2["time_ms_eur"]
+    # avg_ger["num_nodes_searched_2"] = avg_ger_2["num_nodes_searched"]
+
+    order = [
+        "dijkstra",
+        "astar_chpot",
+        "dijkstra_bidir",
+        "astar_bidir_chpot",
+        "core_ch",
+        "core_ch_chpot",
+    ]
+
+    avg_all = avg_all.reindex(order)
+
+    # HERE ORDER OF COLUMNS
+    values = (
+        # avg_all[["time_ms", "time_ms_2", "num_nodes_searched", "num_nodes_searched_2"]]
+        avg_all[["time_ms", "time_ms_2", "time_ms_eur", "time_ms_eur_2"]]
+        .to_numpy()
+        .flatten()
+    )
+
+    with open(
+        os.path.join(LATEX_GEN_PATH, "eval_median_running_times_all-TEMPLATE.tex")
+    ) as f:
+        template = f.read()
+
+    if len(values) != template.count("@"):
+        print(
+            "ERROR: NUMBER OF VALUES ("
+            + str(len(values))
+            + ") DOES NOT EQUAL NUMBER OF BLANKS ("
+            + str(template.count("@"))
+            + ") FOR TEMPLATE "
+            + name_ger
+        )
+        return
+
+    i = 0
+
+    value_to_string = lambda x: "-" if np.isnan(x) else "{:.2f}".format(round(x, 2))
+
+    for m in reversed(list(re.finditer("@", template))):
+        template = (
+            template[: m.start(0)]
+            + value_to_string(values[len(values) - 1 - i])
+            + template[m.end(0) :]
+        )
+        i += 1
+
+    with open(
+        os.path.join(LATEX_GEN_PATH, "eval_median_running_times_all.tex"), "w"
+    ) as f:
+        f.write(template)
+
+
+def run_all_times_no_path():
+    name_eur = "thesis_avg_mid-csp"
+    name_eur_2 = "thesis_avg_mid-csp_2"
+
+    avg_eur = read_measurement(name_eur + "-" + "parking_europe_hgv")
+    avg_eur_2 = read_measurement(name_eur_2 + "-" + "parking_europe_hgv")
+
+    # avg_eur = avg_eur.groupby(["algo"]).median()
+    # avg_eur_2 = avg_eur_2.groupby(["algo"]).median()
+
+    # avg_eur["time_ms_eur_2"] = avg_eur_2["time_ms_eur"]
+    # avg_ger["num_nodes_searched_2"] = avg_ger_2["num_nodes_searched"]
+
+    longest = pd.DataFrame()
+    longest_2 = pd.DataFrame()
+
+    order = [
+        "dijkstra",
+        "astar_chpot",
+        "dijkstra_bidir",
+        "astar_bidir_chpot",
+        "core_ch",
+        "core_ch_chpot",
+    ]
+
+    for algo in order:
+        # num_rows = avg_eur.loc[avg_eur["algo"] == algo].size[0] / 10
+        # longest = longest.append(avg_eur.loc[avg_eur["algo"] == algo].sort_values("time_ms")[-num_rows:])
+        subset = avg_eur.loc[avg_eur["algo"] == algo]
+        longest = longest.append(subset.loc[np.isnan(subset["path_distance"])])
+
+        subset_2 = avg_eur_2.loc[avg_eur_2["algo"] == algo]
+        longest_2 = longest_2.append(subset_2.loc[np.isnan(subset_2["path_distance"])])
+
+    longest["time_ms_2"] = longest_2["time_ms"]
+    longest_mean = longest.groupby("algo").mean()
+    longest_median = longest.groupby("algo").median()
+    longest_mean = longest_mean.reindex(order)
+    longest_median = longest_median.reindex(order)
+
+    longest_all = pd.DataFrame()
+    longest_all["mean"] = longest_mean["time_ms"]
+    longest_all["median"] = longest_median["time_ms"]
+    longest_all["mean_2"] = longest_mean["time_ms_2"]
+    longest_all["median_2"] = longest_median["time_ms_2"]
+
+    # HERE ORDER OF COLUMNS
+    values = (
+        # avg_eur[["time_ms", "time_ms_2", "num_nodes_searched", "num_nodes_searched_2"]]
+        longest_all[["mean", "mean_2", "median", "median_2"]]
+        .to_numpy()
+        .flatten()
+    )
+
+    with open(
+        os.path.join(LATEX_GEN_PATH, "eval_running_times_no_path-TEMPLATE.tex")
+    ) as f:
+        template = f.read()
+
+    if len(values) != template.count("@"):
+        print(
+            "ERROR: NUMBER OF VALUES ("
+            + str(len(values))
+            + ") DOES NOT EQUAL NUMBER OF BLANKS ("
+            + str(template.count("@"))
+            + ") FOR TEMPLATE "
+            + "running_times_no_path"
+        )
+        return
+
+    i = 0
+
+    value_to_string = lambda x: "-" if np.isnan(x) else "{:.2f}".format(round(x, 2))
+
+    for m in reversed(list(re.finditer("@", template))):
+        template = (
+            template[: m.start(0)]
+            + value_to_string(values[len(values) - 1 - i])
+            + template[m.end(0) :]
+        )
+        i += 1
+
+    with open(os.path.join(LATEX_GEN_PATH, "eval_running_times_no_path.tex"), "w") as f:
+        f.write(template)
+
+
 def run_avg_opt():
     name = "thesis_avg_opt-csp"
     name_2 = "thesis_avg_opt-csp_2"
@@ -253,76 +417,54 @@ def run_avg_opt():
         f.write(template)
 
 
-# def run_avg_fast_times(graph):
-#     name = "thesis_avg_fast-csp"
-#     name_2 = "thesis_avg_fast-csp_2"
+def plot_breaks_running_times(graph):
+    problem = "csp"
+    name = "thesis_rank_times_all-" + problem + "-" + graph
+    name_2 = "thesis_avg_all-" + problem + "-" + graph
 
-#     avg_fast = read_measurement(name + "-" + graph)
-#     avg_fast_2 = read_measurement(name_2 + "-" + graph)
+    queries_all = read_measurement(name).append(read_measurement(name_2))
 
-#     avg_fast = avg_fast.groupby(["algo"]).mean()
-#     avg_fast_2 = avg_fast.groupby(["algo"]).mean()
+    # plot only 2^10 and larger
+    queries_all = queries_all.loc[queries_all["dijkstra_rank_exponent"] >= 10]
 
-#     avg_fast["time_ms_2"] = avg_fast_2["time_ms"]
-#     # avg_fast["num_nodes_searched_2"] = avg_fast_2["num_nodes_searched"]
+    colors = ggPlotColors(4)
+    to_plot = [
+        ("time_ms", "log"),
+    ]
 
-#     order = [
-#         # "astar_bidir_chpot",
-#         "core_ch",
-#         "core_ch_chpot",
-#     ]
+    # algos = ["astar_chpot", "astar_bidir_chpot", "core_ch", "core_ch_chpot"]
+    algos = ["astar_chpot", "core_ch", "core_ch_chpot"]
 
-#     avg_fast = avg_fast.reindex(order)
+    for algo in algos:
+        queries = queries_all.loc[queries_all["algo"] == algo]
+        # queries = queries.loc[queries["path_distance"] == -1]
+        for (column_name, plot_scale) in to_plot:
+            if column_name in queries.columns:
+                fig, ax = plt.subplots(figsize=(10, 5))
+                bp = queries.boxplot(ax=ax, by="path_number_pauses", column=column_name)
 
-#     # HERE ORDER OF COLUMNS
-#     values = (
-#         # avg_fast[["time_ms", "time_ms_2", "num_nodes_searched", "num_nodes_searched_2"]]
-#         avg_fast[["time_ms", "time_ms_2"]]
-#         .to_numpy()
-#         .flatten()
-#     )
+                bp.get_figure().gca().set_title("")
+                fig.suptitle("")
 
-#     with open(
-#         os.path.join(LATEX_GEN_PATH, "eval_running_times_fast-TEMPLATE.tex")
-#     ) as f:
-#         template = f.read()
+                ax.set_xlabel("number of breaks")
+                ax.set_ylabel(column_name)
+                ax.set_yscale(plot_scale)
 
-#     if len(values) != template.count("@"):
-#         print(
-#             "ERROR: NUMBER OF VALUES ("
-#             + str(len(values))
-#             + ") DOES NOT EQUAL NUMBER OF BLANKS ("
-#             + str(template.count("@"))
-#             + ") FOR TEMPLATE "
-#             + name
-#         )
-#         return
+                if plot_scale == "linear":
+                    ax.set_ylim(bottom=-0.1)
+                    ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
-#     i = 0
-
-#     for m in reversed(list(re.finditer("@", template))):
-#         template = (
-#             template[: m.start(0)]
-#             + "{:.2f}".format(round(values[len(values) - 1 - i], 2))
-#             + template[m.end(0) :]
-#         )
-#         i += 1
-
-#     with open(os.path.join(LATEX_GEN_PATH, "eval_running_times_fast.tex"), "w") as f:
-#         f.write(template)
-
-
-def plot_query_insights(graph):
-    # name = "thesis_avg_all-csp"
-    name_2 = "thesis_avg_all-csp_2"
-    chpot_corech_2 = read_measurement(name_2 + "-" + graph)["core_ch_chpot"]
-
-    with open(os.path.join(LATEX_GEN_PATH, "eval_running_times_fast.tex"), "w") as f:
-        f.write("c")
+                plt.title("breaks_running_times" + "-" + algo + ": " + graph)
+                fig.tight_layout()
+                print("breaks_running_times" + "-" + algo + "-" + column_name + ".png")
+                write_plt(
+                    "breaks_running_times" + "-" + algo + "-" + column_name + ".png",
+                    graph,
+                )
 
 
 def plot_all_rank_times(problem, graph):
-    name = "thesis_avg_all-" + problem + "-" + graph
+    name = "thesis_rank_times_all-" + problem + "-" + graph
 
     queries_all = read_measurement(name)
 
@@ -330,7 +472,6 @@ def plot_all_rank_times(problem, graph):
     queries_all = queries_all.loc[queries_all["dijkstra_rank_exponent"] >= 10]
 
     colors = ggPlotColors(4)
-
     to_plot = [
         ("time_ms", "log"),
     ]
@@ -351,9 +492,10 @@ def plot_all_rank_times(problem, graph):
                 bp.get_figure().gca().set_title("")
                 fig.suptitle("")
 
-                ax.set_xlabel("dijkstra rank")
+                ax.set_xlabel("Dijkstra Rank")
                 ax.set_ylabel(column_name)
                 ax.set_yscale(plot_scale)
+                ax.set_ylabel("Running Time [ms]")
 
                 if plot_scale == "linear":
                     ax.set_ylim(bottom=-0.1)
@@ -363,13 +505,19 @@ def plot_all_rank_times(problem, graph):
                     ax.xaxis, queries["dijkstra_rank_exponent"].unique()
                 )
 
-                plt.title(name + "-" + algo + ": " + graph)
+                # plt.title(name + "-" + algo + ": " + graph)
+                plt.title("")
                 fig.tight_layout()
-
                 write_plt(name + "-" + algo + "-" + column_name + ".png", graph)
 
 
 if __name__ == "__main__":
     run_avg_all_times()
+    run_median_all_times()
     run_avg_opt()
+
+    run_all_times_no_path()
+
+    plot_breaks_running_times("parking_europe_hgv")
     plot_all_rank_times("csp", "parking_europe_hgv")
+    plot_all_rank_times("csp_2", "parking_europe_hgv")
